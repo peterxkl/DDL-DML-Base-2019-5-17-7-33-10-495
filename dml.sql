32c1e5c9f064aa925717dba5efb93059a4528907
peterxkl <32321026+peterxkl@users.noreply.github.com>
2019-07-02 10:14:03 +0800

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- 插入记录  
-
+insert into student values('001','张三',18,'男'),('002','李四',20,'女');
 -- 修改记录  
-
+update student set age=22 where id='001';
 --  删除记录  
-
+delete from student where id='001';
 -- 查询记录  
+selete * from student;
